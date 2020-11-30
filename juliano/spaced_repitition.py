041669206297e4e2ac43093c9849680af389d185
Martin Bierbaum <martin.bierbaum@rub.de>
2020-12-01 00:38:13 +0100

    convert sm-2 formula into lookup table

--- juliano/spaced_repitition.py
+++ juliano/spaced_repitition.py
@@ -38,9 +38,18 @@ def update_item(db_session, item, grade):
             item.inter_repitition_interval * item.easiness_factor
         )
 
-    item.easiness_factor = item.easiness_factor + (
-        0.1 - (5 - grade) * (0.08 + (5 - grade) * 0.02)
+    increment = dict(
+        [
+            (0, -0.8),
+            (1, -0.54),
+            (2, -0.32),
+            (3, -0.14),
+            (4, 0),
+            (5, 0.1),
+        ]
     )
+
+    item.easiness_factor += increment[grade]
 
     if item.easiness_factor < 1.3:
         item.easiness_factor = 1.3
